b6d107d14bf7317a4acd9995aab3491831f5650c
mohamedsalah173 <mohamedsalahmasoud173@gmail.com>
2025-01-15 02:45:47 +0200

    add authority level on endpoints for users and admins

--- src/main/java/com/example/taco_security/config/SecurityConfig.java
+++ src/main/java/com/example/taco_security/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,12 +17,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests
+                        .requestMatchers("/home").permitAll()
+                        .anyRequest().authenticated()
+        );
 //        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
--- src/main/java/com/example/taco_security/controller/HomeController.java
+++ src/main/java/com/example/taco_security/controller/HomeController.java
@@ -1,12 +1,27 @@
 package com.example.taco_security.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String index(){
         return "home";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/home")
+    public String home(){
+        return "Available for all users";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/secureHome")
+    public String secureHome(){
+        return  "Available For Admin Users Only";
     }
 }
